735df17b17562fe0a11bfba3ec3994e09f4a0060
FatihUtkuKara <fatihutkukara@gmail.com>
2023-07-26 12:12:01 +0300

    swipe carousel

--- relateddigital-google/src/main/java/com/relateddigital/relateddigital_google/inapp/swipecarousel/SwipeCarouselView.kt
+++ relateddigital-google/src/main/java/com/relateddigital/relateddigital_google/inapp/swipecarousel/SwipeCarouselView.kt
@@ -79,7 +79,7 @@ class SwipeCarouselView @JvmOverloads constructor(
         }
 
         expandableView.visibility = View.VISIBLE
-        expandableView.alpha = 0f
+        expandableView.alpha = 1f
         expandableView.translationY = -targetHeight.toFloat()
 
         val slideDownAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_down)
@@ -98,8 +98,6 @@ class SwipeCarouselView @JvmOverloads constructor(
             })
 
         imageView.isEnabled = false
-        //val rotateAnimation = AnimationUtils.loadAnimation(context, R.anim.rotate_down)
-        //imageView.startAnimation(rotateAnimation)
         imageView.visibility = GONE
     }
 
@@ -118,7 +116,7 @@ class SwipeCarouselView @JvmOverloads constructor(
         expandableView.startAnimation(slideUpAnimation)
 
         expandableView.animate()
-            .alpha(0f)
+            .alpha(1f)
             .translationY(-initialHeight.toFloat())
             .setDuration(1200)
             .setInterpolator(AccelerateDecelerateInterpolator())
