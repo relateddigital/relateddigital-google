2022fb717bbb9e0880dbf9a926644c676702591a
FatihUtkuKara <fatihutkukara@gmail.com>
2023-10-25 15:14:31 +0300

    cancel mini drawCircle function

--- relateddigital-google/src/main/java/com/relateddigital/relateddigital_google/inapp/inappmessages/MiniCircleImageView.kt
+++ relateddigital-google/src/main/java/com/relateddigital/relateddigital_google/inapp/inappmessages/MiniCircleImageView.kt
@@ -37,10 +37,10 @@ class MiniCircleImageView : AppCompatImageView {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        val centerX = mCanvasWidth.toFloat() / 2.toFloat()
-        val centerY = mCanvasHeight.toFloat() / 2.toFloat()
-        val radius = 0.7f * centerX.coerceAtMost(centerY)
-        canvas.drawCircle(centerX, centerY, radius, mWhitePaint!!)
+        val centerX = (mCanvasWidth.toFloat() / 2.toFloat())
+        val centerY = (mCanvasHeight.toFloat() / 2.toFloat())
+        val radius = 1.4f * centerX.coerceAtMost(centerY)
+       // canvas.drawCircle(centerX, centerY, radius, mWhitePaint!!)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
